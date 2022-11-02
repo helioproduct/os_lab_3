08ffc27fb6fb20e90e32bdab78129a248142c922
helio <helioproduct.sync@gmail.com>
2022-11-02 12:45:49 +0300

    test.py updated with cli args

--- src/task/test.py
+++ src/task/test.py
@@ -1,15 +1,14 @@
 #!/bin/python3
 
 import re
+from sys import argv
 
-file = open("./source.txt", "r")
+
+file = open(argv[1], "r")
 s = file.read()
 file.close()
 
-result = [m.start() for m in re.finditer('секс', s)]
-
-print(len(result))
-print()
+result = [m.start() for m in re.finditer(argv[2], s)]
 
 for i in result:
     print(i)
